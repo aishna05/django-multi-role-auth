448cfdf9474cf78b40f08f1b49f48150eccee123
aishna05 <160373169+aishna05@users.noreply.github.com>
2025-05-16 22:12:29 +0530

    final done

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('patient/dashboard/', views.patient_dashboard, name='patient_dashboard'),
     path('doctor/dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
